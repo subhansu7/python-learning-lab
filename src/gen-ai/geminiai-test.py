1e3190055f8e020d64b03328a2ba5375901621e2
subhansu <sarangisubhansu7@gmail.com>
2026-03-22 16:50:50 +0530

    geminiai test code

--- src/gen-ai/geminiai-test.py
+++ src/gen-ai/geminiai-test.py
@@ -1,16 +1,17 @@
-"""
 import google.generativeai as genai
+
 import os
 
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-"""
-from google import genai
-import os
+model = genai.GenerativeModel("gemini-2.5-flash")
+response = model.generate_content("What is AI?")
+print(response.text)
 
-client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+#client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
 # You can set the GEMINI_API_KEY environment variable to your API key before running this code.
-
+"""
 model = genai.GenerativeModel("gemini-2.5-flash")
 response = model.generate_content("Explain AI in simple words")
 print(response.text)
+"""
